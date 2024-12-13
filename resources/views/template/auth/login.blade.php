3c1cefb2d4c5ae54b8b722af9e63a749ee569705
ahmadrisyfa7 <ahmadrisyfa123123@gmail.com>
2024-12-14 00:18:50 +0700

    add perbaikan

--- resources/views/template/auth/login.blade.php
+++ resources/views/template/auth/login.blade.php
@@ -111,7 +111,7 @@
                 <!-- You can delete the links only if you purchased the pro version. -->
                 <!-- Licensing information: https://bootstrapmade.com/license/ -->
                 <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-                Designed by <a href="{{url('/')}}">Pertpustakan</a>
+                Designed by <a href="{{url('/')}}">Perpustakaan</a>
               </div>
 
             </div>
